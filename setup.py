3517fb5a3bbf0893b1d35adf350130df0238e233
Ian Chen <ianre657@gmail.com>
2020-02-29 00:15:57 +0800

    long description

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name="hugo-deployer-git",
-    version="0.87",
+    version="0.0.1",
     license='MIT',
     url='https://github.com/ianre657/hugo-deployer-git',
     packages=find_packages( exclude="test-site"),
     author="Ian Chen",
     author_email="ianre657@gmail.com",
     description="Deploy your hugo site with github pages using one command",
+    long_description=long_description,
     keywords= ["hugo", "github pages", "deploy", "git", "blogger", "utility", "static site"],
     classifiers=[
       "Programming Language :: Python :: 3",
